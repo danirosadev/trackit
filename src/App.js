8f3041548515716d61ddcc054d0d2a2a44b4c12c
Dani Rosa <danirosa.dev@gmail.com>
2022-12-16 20:01:19 -0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import GlobalStyle from "./assets/GlobalStyle";
+import GlobalStyle from "./assets/GlobalStyle"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react";
+import { UserContext } from "../src/context/context"
+import SignInPage from "./pages/SingInPage/SignInPage"
+import SignUpPage from "./pages/SignUpPage/SignUpPage"
+import HabitsPage from "./pages/HabitsPage/HabitsPage"
+import TodayPage from "./pages/TodayPage/TodayPage"
+import HistoryPage from "./pages/HistoryPage/HistoryPage"
 
 function App() {
+  const [userData, setUserData] = useState()
+
   return (
-    <>
-      <GlobalStyle />
-      App
-    </>
+    <UserContext.Provider value={{ userData, setUserData }} >
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
+          <Route path="/habitos" element={<HabitsPage />} />
+          <Route path="/hoje" element={<TodayPage />} />
+          <Route path="/historico" element={<HistoryPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- src/pages/SingInPage/SignInPage.js
+++ src/pages/SingInPage/SignInPage.js
@@ -1,5 +1,71 @@
-export default function SignInPage(){
-    return(
-        <>SignInPage</>
+import { Container, FormContainer, Input, Button } from "./style"
+import { Link, useNavigate } from "react-router-dom"
+import logo from "../../assets/img/logo.png"
+import axios from "axios"
+import { useState, useContext } from "react"
+import { ThreeDots } from "react-loader-spinner"
+import { UserContext } from "../../context/context"
+import {BASE_URL} from "../../constants/urls"
+
+export default function SignInPage() {
+    const { setUserData } = useContext(UserContext)
+    const navigate = useNavigate()
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+    const [disabled, setDisabled] = useState(false)
+
+    function handleForm(e) {
+        e.preventDefault()
+
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function login(e) {
+        e.preventDefault()
+
+        const promise = axios.post(`${BASE_URL}/auth/login`, form)
+        promise.then((res) => {
+            navigate("/hoje")
+            setUserData(res.data)
+            console.log(res.data)
+            setDisabled(true)
+        })
+        promise.catch((err) => {
+            alert(err.response.data.message)
+            setDisabled(false)
+        })
+    }
+
+
+    return (
+        <Container>
+            <img src={logo} alt="logo" />
+            <FormContainer>
+                <form onSubmit={login}>
+                    <Input data-test="email-input" name="email" onChange={handleForm} value={form.email} disabled={disabled} type="email" placeholder="email" required />
+                    <Input data-test="password-input" name="password" onChange={handleForm} value={form.password} disabled={disabled} type="password" placeholder="senha" required />
+                    <Button data-test="login-btn" disabled={disabled} type="submit">
+                        {disabled ? <ThreeDots
+                            height="80"
+                            width="80"
+                            radius="9"
+                            color="#ffffff"
+                            ariaLabel="three-dots-loading"
+                            wrapperStyle={{}}
+                            wrapperClassName=""
+                            visible={true}
+                        /> : <p>Entrar</p>}
+                    </Button>
+                </form>
+                <Link to="/cadastro">
+                    <p data-test="signup-link">Não tem uma conta? Cadastre-se!</p>
+                </Link>
+            </FormContainer>
+        </Container>
     )
 }
