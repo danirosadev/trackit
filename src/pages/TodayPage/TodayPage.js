2431c0cc37af11ab816fe6aa8dc5afa62276c33a
Dani Rosa <danirosa.dev@gmail.com>
2022-12-20 10:03:42 -0300

    test3

--- src/pages/TodayPage/TodayPage.js
+++ src/pages/TodayPage/TodayPage.js
@@ -1,4 +1,4 @@
-import {Container, TitleContainer, Title, SubTitle} from "./style"
+import { Container, TitleContainer, Title, SubTitle } from "./style"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
 import TodayCard from "../../components/TodayCard"
@@ -7,20 +7,20 @@ import 'dayjs/locale/pt-br'
 import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../../context/context"
 import axios from "axios"
-import {BASE_URL} from "../../constants/urls"
+import { BASE_URL } from "../../constants/urls"
 
-export default function TodayPage(){
+export default function TodayPage() {
     const [todayHabits, setTodayHabits] = useState([])
-    const {userData} = useContext(UserContext)
+    const { userData } = useContext(UserContext)
 
     let weekday = ''
     let day = ''
 
-    function takeDate(){
+    function takeDate() {
         weekday = dayjs().locale('pt-br').format('dddd').replaceAll('-feira', '');
         weekday = weekday.replace(/^./, weekday[0].toUpperCase());
         day = dayjs().format('DD/MM');
-        return {weekday, day}; 
+        return { weekday, day };
     }
     takeDate()
 
@@ -36,29 +36,31 @@ export default function TodayPage(){
             console.log(res.data)
             setTodayHabits(res.data)
         })
-        .catch((err) => {
-            console.log(err.response.data)
-        })
+            .catch((err) => {
+                console.log(err.response.data)
+            })
     }, [])
 
-    return(
+    return (
         <>
             <Header />
-            <Container data-test="today-habit-container">
+            <Container>
                 <TitleContainer>
                     <Title data-test="today">{weekday}, {day}</Title>
                     <SubTitle data-test="today-counter">Nenhum hábito concluído ainda</SubTitle>
                 </TitleContainer>
-                {todayHabits.map((hab) => {
-                    <TodayCard 
-                    key={hab.id}
-                    habId={hab.id}
-                    name={hab.name}
-                    done={hab.done}
-                    currentSequence={hab.currentSequence}
-                    highestSequence={hab.highestSequence}
-                    />
-                })}
+                <div  data-test="today-habit-container">
+                    {todayHabits.map((hab) => {
+                        <TodayCard
+                            key={hab.id}
+                            habId={hab.id}
+                            name={hab.name}
+                            done={hab.done}
+                            currentSequence={hab.currentSequence}
+                            highestSequence={hab.highestSequence}
+                        />
+                    })}
+                </div>
             </Container>
             <Footer />
         </>
